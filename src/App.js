b87ce140bac569d351d12d1b23683dfa80d5cd34
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-11-23 13:06:08 +0200

    Create Basic Routes with the React Router v4 BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom'
-console.log(BrowserRouter)
-const App = () => <h1>Hello, World!</h1>
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+const Home = (props) => {
+  console.log(props)
+  return <h1>Home</h1>
+}
+
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path='/' component={Home} />
+      {/* <Route path='/about' render={() => <h1>About</h1>} /> */}
+      <Route
+        path='/about'
+        children={({match}) => match && <h1>About</h1>} />
+    </div>
+  </Router>
+)
 
 export default App
