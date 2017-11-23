99c6b628d11a35e3d8edf7197adaf8cd2c2651a8
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-11-23 14:47:51 +0200

    Use URL Parameters with React Router v4 - react Video Tutori

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
-const isActiveFunc = (match, location) => {
-  console.log(match, location)
-  return false
-}
-
-const Links = () => {
-  return(
-    <nav>
-      <NavLink activeClassName="active" to="/">Home</NavLink>
-      <NavLink activeStyle={{color: 'green'}} to="/about">About</NavLink>
-      <NavLink
-        isActive={isActiveFunc}
-        to="/contact">Contact
-      </NavLink>
-    </nav>
-  )
-}
-
-const App = () => (
+const App = (props) => (
   <Router>
     <div>
-      <Links />
-      <Route exact path='/' render={() => <h1>Home</h1>} />
-      <Route path='/about' render={() => <h1>About</h1>} />
-      <Route path='/contact' render={() => <h1>Contact</h1>} />
+      <Route path='/:page?/:subpage?' render={({match}) => (
+        <h1>
+          PAGE: {match.params.page || 'Home'}<br />
+          SUBPAGE: {match.params.subpage}
+        </h1>
+      )} />
     </div>
   </Router>
 )
