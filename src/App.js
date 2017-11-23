d5d69f6e46cede41c718e1f0e4a6b7e599babe35
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-11-23 18:05:04 +0200

    Render Multiple Components for the Same Route in React Route

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
 
 const Links = () => {
   return(
     <nav>
-      <Link to="/">Home</Link>
+      <Link to="/home">Home</Link>
       <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+      <Header />
+      <Content />
     </nav>
   )
 }
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Links />
-          <Switch>
-            <Route exact path='/' render={() => <h1>Home</h1>} />
-            <Route path='/about' render={() => <h1>About</h1>} />
-            <Route path='/contact' render={() => <h1>Contact</h1>} />
-            <Route
-              path='/:itemid'
-              render={({match}) => <h1>Item {match.params.itemid}</h1>} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
+const Header = ({match}) => (
+  <header className="header">
+    <Route
+      path='/:page'
+      render={({match}) => (
+        <h1>{match.params.page} header</h1>
+      )} />
+  </header>
+)
+
+const Content = ({match}) => (
+  <section className="content">
+    <Route
+      path='/:page'
+      render={({match}) => (
+        <p>{match.params.page} content</p>
+      )} />
+  </section>
+)
+
+const App = (props) => (
+  <Router>
+    <div>
+      <Links />
+    </div>
+  </Router>
+)
 
 export default App
