e3565102d77922f8ea2cb4f3dcfa44bcc4ece148
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-11-23 14:51:27 +0200

    Use Regular Expressions with Routes with React Router v4

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 const App = (props) => (
   <Router>
     <div>
-      <Route path='/:page?/:subpage?' render={({match}) => (
+      <Route path='/:a/:b' render={({match}) => (
         <h1>
-          PAGE: {match.params.page || 'Home'}<br />
-          SUBPAGE: {match.params.subpage}
+          paramA: {match.params.a}<br />
+          paramB: {match.params.b}
         </h1>
       )} />
     </div>
