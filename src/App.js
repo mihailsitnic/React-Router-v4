bfc88ec1dca795639b8debf8f714d1803d9cd369
Mihail Sitnic <mihail.sitnic@gmail.com>
2017-11-23 23:46:47 +0200

    Understand the Difference Between the React Router v4 Router

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Link, Prompt} from 'react-router-dom';
+import React from 'react';
+import {BrowserRouter as Router, Route, Link, BrowserRouter, HashRouter, MemoryRouter, StaticRouter, NativeRouter} from 'react-router-dom';
 import './App.css';
 
-const Home = () => (<h1>Home</h1>)
-class Form extends Component {
-  state = {dirty: false}
-  setDirty = () => this.setState({dirty: true})
-  render() {
-    return(
-      <div>
-        <h1>Form</h1>
-        <input type="text" onInput={this.setDirty} />
-        <Prompt
-          when={this.state.dirty}
-          message="Data will be lost!" />
-      </div>
-    )
-  }
-}
-const App = () => (
-  <Router>
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/form">Form</Link>
-      <Route exact path="/" component={Home} />
-      <Route path="/form" component={Form} />
-    </div>
-  </Router>
+
+const LinkRoutes = () => (
+  <div>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Route exact path="/" render={() => <h1>Home</h1>} />
+    <Route path="/about" render={() => <h1>About</h1>} />
+  </div>
 )
 
-export default App
+const forceRefresh = () => {
+  console.log(new Date())
+  return false
+}
+
+const BrowserRouterApp = () => (
+  <BrowserRouter forceRefresh={forceRefresh()}>
+    <LinkRoutes />
+  </BrowserRouter>
+)
+
+const HashRouterApp = () => (
+  <BrowserRouter hashType="hashbang">
+    <LinkRoutes />
+  </BrowserRouter>
+)
+
+const MemoryRouterApp = () => (
+  <MemoryRouter
+    initialEntries={['/', '/about']}
+    initialIndex={0}
+    >
+    <LinkRoutes />
+  </MemoryRouter>
+)
+
+const StaticRouterApp = () => (
+  <StaticRouter location="/about" context={{}}>
+    <LinkRoutes />
+  </StaticRouter>
+)
+
+export default StaticRouterApp
